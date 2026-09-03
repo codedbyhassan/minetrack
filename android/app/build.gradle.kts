c2f82d9d7841d5a47b4f520391f137980e64876a
Hassan Agyemang Boakye <128396960+codedbyhassan@users.noreply.github.com>
2026-09-03 15:21:50 +0000

    fix(android): correct Kotlin Gradle import order

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,10 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
-
-import java.util.Properties
 
 android {
     namespace = "com.codedbyhassan.minetrack"
